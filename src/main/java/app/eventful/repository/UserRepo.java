99c9d6c3a8faf64be9d6cd04efb40fec8c3fc916
Andrej Hristov <andrejhris23@gmail.com>
2022-05-12 15:20:02 +0200

    add: find by email sql query

--- src/main/java/app/eventful/repository/UserRepo.java
+++ src/main/java/app/eventful/repository/UserRepo.java
@@ -2,6 +2,10 @@ package app.eventful.repository;
 
 import app.eventful.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepo extends JpaRepository<User, Long> {
+
+    @Query("SELECT DISTINCT u from User u where u.email=?1")
+    User findByEmail(String email);
 }
