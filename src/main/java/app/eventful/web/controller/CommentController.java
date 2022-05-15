acb5996c7f5ab0c663627a064ba1243d8958d35f
Andrej Hristov <andrejhris23@gmail.com>
2022-05-15 19:33:04 +0200

    fix: add cross origin annotation for cors issue

--- src/main/java/app/eventful/web/controller/CommentController.java
+++ src/main/java/app/eventful/web/controller/CommentController.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/comment")
+@CrossOrigin(origins = "http://localhost:3000")
 public class CommentController {
 
     private final CommentService commentService;
--- src/main/java/app/eventful/web/controller/UserController.java
+++ src/main/java/app/eventful/web/controller/UserController.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/user")
+@CrossOrigin(origins = "http://localhost:3000")
 public class UserController {
 
     private final UserService userService;
